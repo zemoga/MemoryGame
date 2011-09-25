754f29cd64e36aff6ebe294c0e9181141818bf3d
Andres Garcia <andres@zemoga.com>
2011-09-24 19:40:21 -0500

    fixes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function randOrd(){
 	return (0.5 - Math.random()); 
 }
 
-var chars = ['A','&infin;','C','D','&times;','&oplus;','&#x272D;'];
+var chars = ['A','&infin;','C','D','&times;','&oplus;','&#x272D;', '&hearts;'];
 var board = document.getElementById('game-board');
 
 function initBoard(){
@@ -89,8 +89,9 @@ function getContentByCard(card){
 
 function resetGameHandler(event){
 	 if(matched == chars.length){
-    	alert("YOU WIN !!");
+    	alert("FELICITACIONES\rJuega de nuevo");
     	removeCards();
+    	matched = 0;
 	}
 }
 
@@ -131,5 +132,43 @@ function checkClickedCards(){
     }
 }
 
-startGame();
+
+/* Interface handlers */
+document.getElementById("init-button").addEventListener('click', 
+				function(){
+					document.getElementById('sections-container').style.left = '-640px';
+				}, false);
+				
+document.getElementById("instructions-button").addEventListener('click', 
+				function(){
+					document.getElementById('sections-container').style.left = '-320px';
+				}, false);
+				
+document.getElementById("score-button").addEventListener('click', 
+				function(){
+					document.getElementById('sections-container').style.left = '-960px';
+				}, false);
+				
+
+
+
+var backButtons = document.getElementsByClassName('back-button'); 
+
+for(var i = 0; i < backButtons.length; i++){
+	backButtons[i].addEventListener('click', function(){
+		document.getElementById('sections-container').style.left = '0';			
+	}, false);
+}
+
+
+window.addEventListener('load', function(){
+	window.scrollTo(0,0);
+	startGame();
+}, false);
+
+
+
+
+
+
 
